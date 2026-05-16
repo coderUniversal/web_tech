e8ae2318ddee98d7a701475b70c5c47bfb500f70
Muhammad Abdullah <maherabdullahikhlaq1098@gmail.com>
2026-05-16 13:36:47 +0500

    assignment 3 with product.ejs

---
+++ Assignment 3/app.js
@@ -0,0 +1,32 @@
+const express = require("express");
+const mongoose = require("mongoose");
+require("dotenv").config();
+
+const app = express();
+
+// Import routes
+const productsRoute = require("./routes/products");
+
+app.set("view engine", "ejs");
+app.use(express.static("public"));
+app.use(express.urlencoded({ extended: true }));
+
+// Connect to MongoDB
+mongoose.connect(process.env.MONGO_URI)
+    .then(() => {
+        console.log("MongoDB connected successfully!");
+    })
+    .catch((err) => {
+        console.log("MongoDB connection failed!", err);
+    });
+
+// Use routes
+app.get("/", (req, res) => {
+    res.render("index");
+});
+
+app.use("/products", productsRoute);
+
+app.listen(3000, () => {
+    console.log("Server running on http://localhost:3000");
+});
